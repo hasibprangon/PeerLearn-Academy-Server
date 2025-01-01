657b524f5c41588c7e1983d58823bafe6cfa55fe
hasibprangon <hasibulhossainp09@gmail.com>
2025-01-01 16:22:19 +0600

    get my submission assignment functionality added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7tqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,9 +31,53 @@ async function run() {
 
     const assignmentCollection = client.db('assignmentHub').collection('assignment');
 
-    app.post('/createAssignment', async(req, res) =>{
+    const submittedAssignmentCollection = client.db('assignmentHub').collection('submitted');
+
+    // createAssignment
+    app.get('/assignments', async (req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/assignments/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/createAssignment', async (req, res) => {
       const query = req.body;
       const result = await assignmentCollection.insertOne(query);
+      res.send(result);
+    });
+
+
+    // submittedAssignment
+
+    app.get('/mySubmission', async (req, res) => {
+      const email = req?.query?.email;
+      const query = { email: email };
+      const result = await submittedAssignmentCollection.find(query).toArray();
+      for (const participant of result) {
+        const query1 = { _id: new ObjectId(participant.assignmentId) };
+        const result1 = await assignmentCollection.findOne(query1);
+        if (result1) {
+          participant.title = result1?.title;
+          participant.status = participant?.status;
+          participant.marks = result1?.marks;
+          participant.obtainMarks = participant?.obtainMarks;
+          participant.feedback = participant?.feedback;
+          participant.imgUrl = result1?.imgUrl
+        }
+      }
+      res.send(result)
+    })
+
+    app.post('/submittedAssignment', async (req, res) => {
+      const query = req.body;
+      const result = await submittedAssignmentCollection.insertOne(query);
       res.send(result);
     })
 
@@ -49,9 +92,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('PeerLearn Server is running');
+  res.send('PeerLearn Server is running');
 });
 
 app.listen(port, () => {
-    console.log(`server is running on port: ${port}`);
+  console.log(`server is running on port: ${port}`);
 })
