9f0bd206099cd20d78eada645f33b2ad0595c346
hasibprangon <hasibulhossainp09@gmail.com>
2025-01-03 21:16:51 +0600

    jwt implemented in the specific assignment

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/assignments/:id', async (req, res) => {
+    app.get('/assignments/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await assignmentCollection.findOne(query);
