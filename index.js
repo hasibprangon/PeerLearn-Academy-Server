d53b646cb48ba4bec398d0ff4722831022447be2
hasibprangon <hasibulhossainp09@gmail.com>
2025-01-03 22:35:06 +0600

    same site added

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ async function run() {
 
       res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict'
       })
         .send({ success: true })
     });
@@ -72,7 +73,8 @@ async function run() {
     app.post('/logout', (req, res) => {
       res.clearCookie('token', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict'
       })
       .send({success: true})
     })
